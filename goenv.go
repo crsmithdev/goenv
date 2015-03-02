76ee07db04113ea4926e2c5907fe8ce1854affdc
Chris Smith <csmith@uber.com>
2015-03-01 20:47:50 -0800

    rework

--- goenv.go
+++ goenv.go
@@ -22,95 +22,64 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
-const template = `
-# This file must be used with "source bin/activate" or ". bin/activate"
-
-if [[ -n "${GOENV+1}" ]]; then
-	deactivate
-fi
-
-export GOENV=__GOENV__
-export GOENV_OLDPS1=$PS1
-export GOENV_OLDGOPATH=$GOPATH
-export GOENV_OLDPATH=$PATH
-
-export GOPATH=$GOENV:$GOPATH
-export PATH="$GOENV/bin:$PATH"
-export PS1="($(basename $GOENV))$PS1"
-
-deactivate() {
-	export PS1=$GOENV_OLDPS1
-	export GOPATH=$GOENV_OLDGOPATH
-	export PATH=$GOENV_OLDPATH
-
-	unset GOENV GOENV_OLDPS1 GOENV_OLDPATH GOENV_OLDGOPATH
-	unset -f deactivate
-}
-`
-
-// writeScript writes the modifed script template.
-func writeScript(goenv, path string) error {
-
-	script := strings.Replace(template, "__GOENV__", goenv, -1)
-	err := ioutil.WriteFile(path, []byte(script), 777)
-
-	return err
+// Command is a command-line action.
+type Command struct {
+	Usage   string
+	Short   string
+	Long    string
+	GetTask func([]string) (Task, error)
 }
 
-// createSubdirs creates the goenv directory structure.
-func createSubdirs(path string, names []string) error {
-
-	for _, name := range names {
-		subDir := filepath.Join(path, name)
-		if err := os.MkdirAll(subDir, os.ModeDir|0755); err != nil {
-			return err
-		}
-	}
-
-	return nil
+// Task is an action.
+type Task interface {
+	Run() error
 }
+
+// a map of command names -> commands.
+var commands map[string]*Command
 
 func init() {
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: goenv DEST_DIR\n")
 	}
+
+	commands = map[string]*Command{
+		"init": &initCommand,
+	}
 }
 
 func main() {
 	flag.Parse()
 
-	if flag.NArg() < 1 {
+	args := flag.Args()
+
+	if len(args) < 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	dir, err := os.Getwd()
+	cmd, found := commands[args[0]]
 
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error getting current directory: %s", err.Error())
+	if !found {
+		fmt.Fprintf(os.Stderr, "goenv: unrecognized command %s\n", args[0])
 		os.Exit(1)
 	}
 
-	path := filepath.Join(dir, flag.Arg(0))
-	err = createSubdirs(path, []string{"src", "bin", "pkg"})
+	task, err := cmd.GetTask(args[1:])
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating directories: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "goenv: failed to parse command %s\n", cmd)
 		os.Exit(1)
 	}
 
-	scriptPath := filepath.Join(path, "bin/activate")
-	err = writeScript(path, scriptPath)
+	err = task.Run()
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing activate script: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "goenv: error running command: %s\n", err)
 		os.Exit(1)
 	}
 }
---
+++ init.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+	"text/template"
+)
+
+const script = `
+# This file must be used with "source activate" or ". activate"
+
+if [[ -n "${GOENV+1}" ]]; then
+	deactivate
+fi
+
+export GOENV {{ .ProjectName }}
+export GOENV_OLDPS1=$PS1
+export GOENV_OLDGOPATH=$GOPATH
+export GOENV_OLDPATH=$PATH
+
+export GOPATH={{ .GoenvDir }}
+export PATH="$GOPATH/bin:$PATH"
+export PS1="($(basename $GOPATH))$PS1"
+
+deactivate() {
+	export PS1=$GOENV_OLDPS1
+	export GOPATH=$GOENV_OLDGOPATH
+	export PATH=$GOENV_OLDPATH
+
+	unset GOENV GOENV_OLDPS1 GOENV_OLDPATH GOENV_OLDGOPATH
+	unset -f deactivate
+}
+`
+
+var initCommand = Command{
+	Usage:   "init usage",
+	Short:   "init short",
+	Long:    "init long",
+	GetTask: NewInitTask,
+}
+
+// InitTask initializes a goenv.
+type InitTask struct {
+	ProjectName string // the name of the project, e.g. "goenv".
+	ProjectPath string // the import path of the project, e.g. "github.com/crsmithdev/goenv"
+	GoenvDir    string // the goenv dir to create, default "~/.goenv/[ProjectName]"
+	ScriptDir   string // the directory in which to write the activate script, default "./goenv"
+	WorkingDir  string // the current working directory.
+}
+
+// NewInitTask returns a new InitTask created with the specified command-line args.
+func NewInitTask(args []string) (Task, error) {
+
+	flags := flag.NewFlagSet("init", flag.ExitOnError)
+
+	goenvDir := flags.String("workspace", "", "the workspace directory")
+	scriptDir := flags.String("script", "./goenv/activate", "the activation script path")
+
+	flags.Parse(args)
+	args = flags.Args()
+
+	if len(args) < 1 {
+		return nil, errors.New("no project name specified")
+	}
+
+	if len(args) < 2 {
+		return nil, errors.New("no project path specified")
+	}
+
+	workingDir, err := os.Getwd()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &InitTask{
+		ProjectName: args[0],
+		ProjectPath: args[1],
+		GoenvDir:    *goenvDir,
+		ScriptDir:   *scriptDir,
+		WorkingDir:  workingDir,
+	}, nil
+}
+
+// Run exeuctes the InitTask
+func (task *InitTask) Run() error {
+
+	fmt.Println("goenv: initializing...")
+
+	if task.GoenvDir == "" {
+		usr, err := user.Current()
+
+		if err != nil {
+			return err
+		}
+
+		task.GoenvDir = filepath.Join(usr.HomeDir, ".goenv/", task.ProjectName)
+	}
+
+	if err := task.makePaths(); err != nil {
+		return err
+	}
+
+	if err := task.writeScript(); err != nil {
+		return err
+	}
+
+	fmt.Println("goenv: done")
+
+	return nil
+}
+
+// makePaths creates directory paths for a goenv.
+func (task *InitTask) makePaths() error {
+
+	wPath := filepath.Join(task.GoenvDir, "src", task.ProjectPath)
+
+	var dirs = []string{
+		filepath.Dir(task.ScriptDir),
+		filepath.Join(task.GoenvDir, "bin"),
+		filepath.Join(task.GoenvDir, "pkg"),
+		wPath,
+	}
+
+	for _, dir := range dirs {
+		err := os.MkdirAll(dir, os.ModeDir|0755)
+
+		fmt.Printf("goenv: created directory %s\n", dir)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	err := os.Remove(wPath)
+	err = os.Symlink(task.WorkingDir, wPath)
+
+	fmt.Printf("goenv: symlinked %s -> %s\n", task.WorkingDir, wPath)
+
+	return err
+}
+
+// writeScript writes the goenv activate script.
+func (task *InitTask) writeScript() error {
+
+	scriptTemplate := template.New("test")
+	scriptTemplate, err := scriptTemplate.Parse(script)
+
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+	err = scriptTemplate.Execute(&buf, task)
+
+	if err != nil {
+		return err
+	}
+
+	_ = os.Remove(task.ScriptDir)
+	err = ioutil.WriteFile(task.ScriptDir, []byte(buf.String()), 777)
+
+	fmt.Printf("goenv: wrote activation script at %s\n", task.ScriptDir)
+
+	return err
+}

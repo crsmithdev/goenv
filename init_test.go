45c5851c3daa008c3e1c819ea6f077b133773b62
Chris Smith <csmith@uber.com>
2015-03-06 08:52:50 -0800

    more tests

--- init_test.go
+++ init_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/user"
+	"path/filepath"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -61,4 +64,31 @@ var _ = Describe("Init", func() {
 		assert.Contains(GinkgoT(), err.Error(), "import")
 	})
 
+	It("Generates an activate script with the specified options", func() {
+
+		wd, _ := os.Getwd()
+		script := filepath.Join(wd, "temp")
+
+		task := &InitTask{
+			GoPath:      "go",
+			ImportPath:  "import/path",
+			ProjectName: "name",
+			ProjectPath: "path",
+			ScriptPath:  script,
+		}
+
+		task.Run()
+
+		bytes, _ := ioutil.ReadFile(script)
+		text := string(bytes)
+
+		assert.Contains(GinkgoT(), text, "export GOENV=name")
+		assert.Contains(GinkgoT(), text, "export GOPATH=go")
+		assert.Contains(GinkgoT(), text, "mkdir -p $(dirname $GOPATH/src/import/path")
+		assert.Contains(GinkgoT(), text, "ln -s path $GOPATH/src/import/path")
+
+		err := os.Remove(script)
+
+		fmt.Println(err)
+	})
 })
